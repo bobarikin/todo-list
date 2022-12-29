a2fcb5527698a09f37f17fc5d22458d6bfce450d
Artyom <a.s.bobarikin@gmail.com>
2022-12-29 13:33:53 +0300

    add wrapper component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from './components/header/Header'
+import { Wrapper } from './components/wrapper/Wrapper'
 
 function App() {
   return (
     <>
       <Header />
+      <Wrapper />
     </>
   )
 }
